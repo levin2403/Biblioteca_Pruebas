a7a32eab2826338e69898ae1abcac391c5953ade
levin2403 <159957744+levin2403@users.noreply.github.com>
2024-11-11 02:01:04 -0700

    Add files via upload

---
+++ ValorationService/src/main/java/valoration/Valorate.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package valoration;
+
+import entityes.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author skevi
+ */
+public class Valorate {
+    
+    /**
+     * 
+     */
+    private static List<Book> books = new ArrayList<>();
+
+    /**
+     * 
+     */
+    public Valorate() {
+    }
+    
+    /**
+     * 
+     * @param book 
+     */
+    public void sentBookToValorate(Book book){
+        
+    }
+    
+    /**
+     * 
+     * @param book 
+     * @param valoration 
+     * @param review 
+     */
+    public void sendValoratedBook(Book book, int valoration, String review){
+        
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public List<Book> getBooks() {
+        return books;
+    }
+    
+    
+}
